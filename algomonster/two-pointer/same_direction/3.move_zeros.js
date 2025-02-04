7f635d2905c909acfcb852e0cb345d2cde8e2eb0
fkurusu7 <coding.fcv@gmail.com>
2025-02-04 16:04:16 -0600

    add move zeros exercise

---
+++ algomonster/two-pointer/same_direction/3.move_zeros.js
@@ -0,0 +1,66 @@
+/** Move Zeros
+  Given an array of integers, move all the 0s to the back of the array 
+  while maintaining the relative order of the non-zero elements. 
+  Do this in-place using constant auxiliary space.
+
+  Input: [1, 0, 2, 0, 0, 7]
+  Output: [1, 2, 7, 0, 0, 0]
+
+ */
+
+function moveZeros(arr) {
+  let slow = 0;
+
+  for (let fast = 0; fast < arr.length; fast++) {
+    if (arr[fast] !== 0) {
+      [arr[slow], arr[fast]] = [arr[fast], arr[slow]];
+      slow++;
+      console.log(arr);
+    }
+  }
+  return arr;
+}
+
+console.log(moveZeros([1, 0, 2, 0, 0, 7])); // [1, 2, 7, 0, 0, 0]
+
+// Inefficient Solution:
+function moveZerosIn(nums) {
+  // copy to new array
+  const nonZeros = [];
+  for (const n of nums) {
+    if (n !== 0) nonZeros.push(n);
+  }
+
+  // Copy back
+  let i = 0;
+  for (; i < nonZeros.length; i++) {
+    nums[i] = nonZeros[i];
+  }
+  // fill the rest with zeros
+  for (; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+}
+let nums = [1, 0, 2, 0, 0, 7];
+moveZerosIn(nums);
+console.log(nums);
+
+function moveZerosCopyInPlace(nums) {
+  let i = 0;
+  // copy in place
+  for (const n of nums) {
+    if (n !== 0) {
+      nums[i] = n;
+      console.log(i);
+      i++;
+    }
+  }
+  // fill the rest with zeros
+  for (; i < nums.length; i++) {
+    console.log(i);
+    nums[i] = 0;
+  }
+}
+nums = [1, 0, 2, 0, 0, 7];
+moveZerosCopyInPlace(nums);
+console.log(nums);
