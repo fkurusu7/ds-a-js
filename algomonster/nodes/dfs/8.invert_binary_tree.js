0062f2fbb86c7fd8e1d5dae0f18a38d0586f72b1
fkurusu7 <coding.fcv@gmail.com>
2025-03-02 00:30:18 -0600

    add test to invert tree exercise

--- algomonster/nodes/dfs/8.invert_binary_tree.js
+++ algomonster/nodes/dfs/8.invert_binary_tree.js
@@ -1,8 +1,10 @@
 /** Invert Binary Tree
 
-Given a binary tree, invert it and return the new value. You may invert it in-place.
+Given a binary tree, invert it and return the new value. 
+You may invert it in-place.
 
-To "invert" a binary tree, switch the left subtree and the right subtree, and invert them both. Inverting an empty tree does nothing.
+To "invert" a binary tree, switch the left subtree and the right subtree, 
+and invert them both. Inverting an empty tree does nothing.
 */
 class Node {
   constructor(val, left = null, right = null) {
@@ -12,16 +14,27 @@ class Node {
   }
 }
 
+const tree = new Node(1);
+tree.left = new Node(2);
+tree.left.left = new Node(4);
+tree.left.left.right = new Node(7);
+tree.left.right = new Node(5);
+
+tree.right = new Node(3);
+tree.right.right = new Node(6);
+tree.right.right.left = new Node(8);
+
 function invertBinaryTree(tree) {
-  if (!tree) return tree;
+  if (tree === null) return tree;
 
-  let left = invertBinaryTree(tree.left);
-  let right = invertBinaryTree(tree.right);
+  const left = invertBinaryTree(tree.left);
+  const right = invertBinaryTree(tree.right);
 
-  tree.right = left;
   tree.left = right;
+  tree.right = left;
 
   return tree;
 }
 
-console.log(invertBinaryTree());
+console.log(tree);
+console.log(invertBinaryTree(tree));
