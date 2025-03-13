f01af9c17757cfcbde12c7abb4af6113746a9a66
fkurusu7 <coding.fcv@gmail.com>
2025-03-12 20:56:05 -0600

    add doubly linked list main functions

--- misc/3.doubly_linked_list.js
+++ misc/3.doubly_linked_list.js
@@ -13,46 +13,92 @@ class DoublyLinkedList {
     this.length = 0; //Tracks list size.
   }
 
+  prettyFormatList() {
+    let current = this.head;
+    let values = [];
+
+    while (current) {
+      values.push(current.value);
+      current = current.next;
+    }
+
+    return values.join(" <-> ");
+  }
+
   append(value) {
     let newNode = new DoublyListNode(value);
 
     if (!this.head) {
       this.head = this.tail = newNode;
     } else {
-      // newNode.next will be null
       this.tail.next = newNode;
       newNode.prev = this.tail;
       this.tail = newNode;
     }
+    this.length++;
+  }
+
+  prepend(value) {
+    let newNode = new DoublyListNode(value);
+
+    if (!this.head) {
+      this.head = this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
 
     this.length++;
+  }
+
+  delete(value) {
+    if (!this.head) return null;
+
+    // if value is in head, check if values are the same
+    // point head to head's next node, and prev to null, decrease length
+    if (this.head.value === value) {
+      this.head = this.head.next;
+      this.head.prev = null;
+      this.length--;
+    }
+
+    // loop until current is null
+    let current = this.head;
+    while (current) {
+      // check if current value is equal to value
+      if (current.value === value) {
+        // check if current is tails, remove current tail and point it to prev
+        if (current === this.tail) {
+          this.tail = this.tail.prev;
+          this.tail.next = null;
+        } else {
+          current.next.prev = current.prev;
+          current.prev.next = current.next;
+        }
+        this.length--;
+        return;
+      }
+      current = current.next;
+    }
   }
 }
 
 const dll = new DoublyLinkedList();
 dll.append("start");
-console.log(dll);
-/* 
-DoublyLinkedList {
-  head: DoublyListNode { value: 'start', next: null, prev: null },
-  tail: DoublyListNode { value: 'start', next: null, prev: null },
-  length: 1
-  }
-  */
+console.log(dll.prettyFormatList()); // 'start'
 dll.append("medium");
-console.log(dll);
-/* 
-DoublyLinkedList {
-  head: DoublyListNode {
-    value: 'start',
-    next: DoublyListNode { value: 'medium', next: null, prev: [Circular] },
-    prev: null
-  },
-  tail: DoublyListNode {
-    value: 'medium',
-    next: null,
-    prev: DoublyListNode { value: 'start', next: [Circular], prev: null }
-  },
-  length: 2
-}
- */
+console.log(dll.prettyFormatList()); // 'start <-> medium'
+dll.append("end");
+console.log(dll.prettyFormatList()); // 'start <-> medium <-> end'
+
+dll.prepend("genesis");
+console.log(dll.prettyFormatList());
+// 'genesis <-> start <-> medium <-> end'
+
+dll.delete("end");
+console.log(dll.prettyFormatList());
+// 'genesis <-> start <-> medium'
+dll.delete("start");
+console.log(dll.prettyFormatList());
+// 'genesis <-> medium'
